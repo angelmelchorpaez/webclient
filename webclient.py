bce56c714088a3bf153749bda3adf9353b9b4a56
angelmelchorpaez <amelchor@martybelcontrol.com>
2025-07-06 19:17:09 +0100

    Descarga foto y almacénala en un archivo foto.png

--- webclient.py
+++ webclient.py
@@ -1,8 +1,13 @@
 import http.client
 
 conexion=http.client.HTTPSConnection(host="www.angelmelchor.pro",port=443)
-conexion.request(method="GET",url="/")
+conexion.request(method="GET",url="/angelmelchor.png")
 respuesta=conexion.getresponse()
 print(f"Respuesta completa: {respuesta}")
 print(f"Propiedades: {respuesta.status}, {respuesta.reason}")
-print(f"Método: {respuesta.read()}")
+if respuesta.status == 200:
+    contenido=respuesta.read()
+    with open("foto.png","wb") as fichero:
+        fichero.write(contenido)
+else:
+    print("ha ocurrido un error accediendo al archivo")
